ce0021fc5a16be1fa43711b468e1e9c9014ab577
BD <33544028+bdphin@users.noreply.github.com>
2022-04-21 11:18:39 +0200

    Added script for auto rebuild dot source file to merged pdf.

---
+++ 图笔记/rebuildAll.sh
@@ -0,0 +1,8 @@
+#用Visual studio code打开本文件，右击选择open in intergrated terminal，然后sh rebuildAll.sh
+
+# 以下命令批量转换gv源文件，生成pdf文件
+dot -Tpdf ./*/*.gv -O
+
+#以下命令合并pdf
+"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o ./分别智品第七.pdf ./7.\ 分别智品第七/*.pdf
+"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o ./分别定品第八.pdf ./8.\ 分别定品第八/*.pdf
