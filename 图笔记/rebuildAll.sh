8fdc4aabe024b1c91ccb3a4ea2a52c105f70c340
BD <33544028+bdphin@users.noreply.github.com>
2022-04-23 09:47:11 +0200

    添加世间品

--- 图笔记/rebuildAll.sh
+++ 图笔记/rebuildAll.sh
@@ -5,5 +5,6 @@ rm ./*/*.pdf
 dot -Tpdf ./*/*.dot -O
 
 #以下命令合并pdf
+"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o ./3.分别世间第三.pdf ./3.\ 分别世间品第三/*.pdf
 "/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o ./7.分别智品第七.pdf ./7.\ 分别智品第七/*.pdf
 "/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o ./8.分别定品第八.pdf ./8.\ 分别定品第八/*.pdf
